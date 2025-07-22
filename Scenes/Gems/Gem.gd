1637816957fc4fae98d05600d1ed8dcb3405c636
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-07-22 16:30:52 -0500

    updated code

--- Scenes/Gems/Gem.gd
+++ Scenes/Gems/Gem.gd
@@ -5,7 +5,7 @@ const SPEED: float = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	DisplayServer.window_set_position(Vector2(300, 300))  # Adjust as needed
+	# DisplayServer.window_set_position(Vector2(300, 300))  # Attempted to have project window open in different monitor
 	pass # Replace with function body.
 
 
@@ -15,7 +15,7 @@ func _process(delta: float) -> void:
 # Gems falls down the screen
 	position.y += SPEED * delta
 	
-	if position.y > get_viewport_rect().end.y:  # This grabs the bottom of the viewport
+	if position.y > get_viewport_rect().end.y:  # This grabs the bottom of the viewport instead of creating a magic number
 		print("Gem falls off.")
 		set_process(false)  # Here we stop the process after each gem falls off the screen.
 		queue_free() # Look deeper into what this means. With this, the gems will actually dissapear from the game.
--- Scenes/Paddle/Paddle.gd
+++ Scenes/Paddle/Paddle.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
+const SPEED: float = 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("move_left") == true: # == true: is not necessary here
+		position.x -= SPEED * delta
+	if Input.is_action_pressed("move_right") == true: 
+		position.x += SPEED * delta
