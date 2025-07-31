8aaa57df509e3a330b6226dbcd44965e58d59d37
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-07-30 21:07:14 -0500

    learn GD scripting syntax

--- Scripting/Basics.gd
+++ Scripting/Basics.gd
@@ -1,6 +1,9 @@
 @tool
 extends EditorScript
 
+var hero: String = "Frodo"
+
 func _run() -> void:  # A colon after the cuntion indicates a 'code block' will follow
 	print("Hello, World.")
+	print(hero)
 	
