5a952b231b554fabe89ea36bf04dcdab98ccf491
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-08-05 18:18:27 -0500

    new

--- Scripting/Basics.gd
+++ Scripting/Basics.gd
@@ -1,9 +1,18 @@
 @tool
 extends EditorScript
 
-var hero: String = "Frodo"
+var hero := "Frodo" # this is shorthand for >  var hero: string = "frodo"
+var age = 24  # this is shorthand for > var age: int = 24
+
+# var hero: string = "Frodo"    >>> this is 
+# var age: int = 24
 
 func _run() -> void:  # A colon after the cuntion indicates a 'code block' will follow
-	print("Hello, World.")
-	print(hero)
+	#print("Hello, World.")
+	print("\n...............")  # New line. we know this from python
+	print(age)
+	print()
+	age = "richard"
+	print(age)
+	print(type_string(typeof(age)))
 	
